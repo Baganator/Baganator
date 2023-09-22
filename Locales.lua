73c63719fd0aee731586829e2315b2b22950c85e
plusmouse <plusmouse@protonmail.com>
2023-09-22 18:51:12 +0100

    Move localisation text file out of source code directory

---
+++ Locales.lua
@@ -0,0 +1,75 @@
+local Locales = {
+  enUS = {},
+  frFR = {},
+  deDE = {},
+  ruRU = {},
+  ptBR = {},
+  esES = {},
+  esMX = {},
+  zhTW = {},
+  zhCN = {},
+  koKR = {},
+  itIT = {},
+}
+
+BAGANATOR_LOCALES = Locales
+
+local L = Locales.enUS
+
+L["BANK"] = "Bank"
+L["REAGENTS"] = "Reagents"
+L["XS_BANK_AND_BAGS"] = "%s's Bank and Bags"
+L["XS_BAGS"] = "%s's Bags"
+L["XS_BANK"] = "%s's Bank"
+L["ALL_CHARACTERS"] = "All Characters"
+
+L["CUSTOMISE_BAGANATOR"] = "Customise Baganator"
+L["RESET_POSITIONS"] = "Reset Positions"
+L["LOCK_BAGS_BANKS_FRAMES"] = "Lock Bags/Bank frames"
+L["CUSTOMISE_REMOVE_BORDERS"] = "Remove borders from Bags/Bank frames"
+L["SHOW_SORT_BUTTON"] = "Show sort button"
+L["CUSTOMISE_EMPTY_SLOTS"] = "Hide empty slots background"
+L["X_TRANSPARENCY"] = "%s%% Transparency"
+L["X_BAG_COLUMNS"] = "%s Bag Columns"
+L["X_BANK_COLUMNS"] = "%s Bank Columns"
+L["X_ICON_SIZE"] = "%spx Icon Size"
+L["SHOW_SORT_BUTTON"] = "Show Sort Button"
+L["SHOW_ITEM_LEVEL"] = "Show equipment item levels"
+L["SHOW_BOE_STATUS"] = "Show equipment BoE status"
+L["CUSTOMISE_SHOW_TABS"] = "Show tabs of recent characters"
+L["SHOW_INVENTORY_IN_TOOLTIPS"] = "Show inventory in item tooltips"
+
+L["INVENTORY_TOTALS_COLON"] = "Inventory Totals:"
+L["BAGS_X_BANKS_X"] = "Bags: %s, Banks: %s"
+L["BAGS_X_BANK_X"] = "Bags: %s, Bank: %s"
+
+L["BOE"] = "BoE"
+
+L["SORT"] = "Sort"
+
+local L = Locales.frFR
+--@localization(locale="frFR", format="lua_additive_table")@
+
+local L = Locales.deDE
+--@localization(locale="deDE", format="lua_additive_table")@
+
+local L = Locales.ruRU
+--@localization(locale="ruRU", format="lua_additive_table")@
+
+local L = Locales.esES
+--@localization(locale="esES", format="lua_additive_table")@
+
+local L = Locales.esMX
+--@localization(locale="esMX", format="lua_additive_table")@
+
+local L = Locales.zhTW
+--@localization(locale="zhTW", format="lua_additive_table")@
+
+local L = Locales.zhCN
+--@localization(locale="zhCN", format="lua_additive_table")@
+
+local L = Locales.koKR
+--@localization(locale="koKR", format="lua_additive_table")@
+
+local L = Locales.itIT
+--@localization(locale="itIT", format="lua_additive_table")@
