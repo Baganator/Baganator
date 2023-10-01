030445aa03cfff1c9211ac0879b5351a752ac8cf
plusmouse <plusmouse@protonmail.com>
2023-10-01 16:56:30 +0100

    Hide 0 counts in tooltip entries for inventory info

--- InventoryTracking/Tooltips.lua
+++ InventoryTracking/Tooltips.lua
@@ -26,11 +26,31 @@ function Baganator.Tooltips.AddLines(tooltip, summaries, itemLink)
     mailCount = mailCount + s.mail
   end
 
-  tooltip:AddLine(BAGANATOR_L_INVENTORY_TOTALS_COLON .. " " .. WHITE_FONT_COLOR:WrapTextInColorCode(BAGANATOR_L_BAGS_X_BANKS_X_MAIL_X:format(bagCount, bankCount, mailCount)))
+  local entries = {}
+  if bagCount > 0 then
+    table.insert(entries, BAGANATOR_L_BAGS_X:format(bagCount))
+  end
+  if bankCount > 0 then
+    table.insert(entries, BAGANATOR_L_BANKS_X:format(bankCount))
+  end
+  if mailCount > 0 then
+    table.insert(entries, BAGANATOR_L_MAILS_X:format(mailCount))
+  end
+  tooltip:AddLine(BAGANATOR_L_INVENTORY_TOTALS_COLON .. " " .. WHITE_FONT_COLOR:WrapTextInColorCode(strjoin(", ", unpack(entries))))
 
   for index = 1, math.min(#tooltipInfo, 4) do
     local s = tooltipInfo[index]
-    tooltip:AddDoubleLine("  " .. s.character, WHITE_FONT_COLOR:WrapTextInColorCode(BAGANATOR_L_BAGS_X_BANK_X_MAIL_X:format(s.bags, s.bank, s.mail)))
+    local entries = {}
+    if s.bags > 0 then
+      table.insert(entries, BAGANATOR_L_BAGS_X:format(s.bags))
+    end
+    if s.bank > 0 then
+      table.insert(entries, BAGANATOR_L_BANK_X:format(s.bank))
+    end
+    if s.mail > 0 then
+      table.insert(entries, BAGANATOR_L_MAIL_X:format(s.mail))
+    end
+    tooltip:AddDoubleLine("  " .. s.character, WHITE_FONT_COLOR:WrapTextInColorCode(strjoin(", ", unpack(entries))))
   end
   if #tooltipInfo > 4 then
     tooltip:AddLine("  ...")
