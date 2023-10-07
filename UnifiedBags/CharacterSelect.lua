03e887c1a7bf2c83837d2130edfe3f67b4e21962
plusmouse <plusmouse@protonmail.com>
2023-10-07 16:50:09 +0100

    Use class colours in character select for bag view

--- UnifiedBags/CharacterSelect.lua
+++ UnifiedBags/CharacterSelect.lua
@@ -1,15 +1,18 @@
 CharacterSelectSidebarMixin = {}
 
+local arrowRight = CreateTextureMarkup("Interface\\AddOns\\Baganator\\Assets\\arrow", 22, 22, 13, 13, 1, 0, 0, 1, 0, 2)
+local arrowLeft = CreateTextureMarkup("Interface\\AddOns\\Baganator\\Assets\\arrow", 22, 22, 13, 13, 0, 1, 0, 1, 0, 2)
+
 function CharacterSelectSidebarMixin:OnLoad()
   self:SetTitle(BAGANATOR_L_ALL_CHARACTERS)
 
   local function UpdateForSelection(frame)
-    if frame:GetText() ~= self.selectedCharacter then
+    if frame.fullName ~= self.selectedCharacter then
       frame:Enable()
-      frame:GetFontString():SetTextColor(1, 1, 1)
+      frame:SetText(frame.fullName)
     else
       frame:Disable()
-      frame:GetFontString():SetTextColor(0, 1, 0)
+      frame:SetText(arrowLeft .. " " .. frame.fullName .. " " .. arrowRight)
     end
   end
 
@@ -18,10 +21,17 @@ function CharacterSelectSidebarMixin:OnLoad()
   view:SetElementInitializer("Button", function(frame, elementData)
     frame:SetNormalFontObject(GameFontHighlight)
     --local fs = frame:CreateFontString(nil, nil, "GameFontHighlight")
-    frame:SetText(elementData)
+    frame.fullName = elementData.fullName
+    frame:SetText(frame.fullName)
+    if elementData.className then
+      local classColor = RAID_CLASS_COLORS[elementData.className]
+      frame:GetFontString():SetTextColor(classColor.r, classColor.g, classColor.b)
+    else
+      frame:GetFontString():SetTextColor(1, 1, 1)
+    end
     --fs:SetAllPoints()
     frame:SetScript("OnClick", function()
-      Baganator.CallbackRegistry:TriggerEvent("CharacterSelect", elementData)
+      Baganator.CallbackRegistry:TriggerEvent("CharacterSelect", elementData.fullName)
     end)
     UpdateForSelection(frame)
   end)
@@ -41,11 +51,7 @@ end
 
 function CharacterSelectSidebarMixin:UpdateList()
   local characters = Baganator.Utilities.GetAllCharacters(self.SearchBox:GetText())
-  local justNames = {}
-  for _, details in ipairs(characters) do
-    table.insert(justNames, details.fullName)
-  end
-  self.ScrollBox:SetDataProvider(CreateDataProvider(justNames))
+  self.ScrollBox:SetDataProvider(CreateDataProvider(characters))
 end
 
 function CharacterSelectSidebarMixin:OnShow()
