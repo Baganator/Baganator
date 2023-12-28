1abd6a2889e5f905a479b3098224cc8e6fac84a2
plusmouse <plusmouse@protonmail.com>
2023-12-28 08:36:00 +0000

    Fix CIMI icons not updating when CIMI settings changed

--- UnifiedBags/ItemButtonDataEvents.lua
+++ UnifiedBags/ItemButtonDataEvents.lua
@@ -1,10 +1,25 @@
 Baganator.Utilities.OnAddonLoaded("Pawn", function()
   -- Equip/unequip
   Baganator.CallbackRegistry:RegisterCallback("EquippedCacheUpdate", function()
-    Baganator.API.RequestItemButtonsRefresh()
+    if Baganator.Config.Get(Baganator.Config.Options.SHOW_PAWN_ARROW) then
+      Baganator.API.RequestItemButtonsRefresh()
+    end
   end)
   -- Spec change
   hooksecurefunc("PawnInvalidateBestItems", function()
-    Baganator.API.RequestItemButtonsRefresh()
+    if Baganator.Config.Get(Baganator.Config.Options.SHOW_PAWN_ARROW) then
+      Baganator.API.RequestItemButtonsRefresh()
+    end
+  end)
+end)
+
+Baganator.Utilities.OnAddonLoaded("CanIMogIt", function()
+  local function Callback()
+    if Baganator.Config.Get(Baganator.Config.Options.SHOW_CIMI_ICON) then
+      Baganator.API.RequestItemButtonsRefresh()
+    end
+  end
+  CanIMogIt:RegisterMessage("OptionUpdate", function()
+    pcall(Callback)
   end)
 end)
