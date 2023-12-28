98060643a37f7d73b16797d6ccc38f490fa2fca8
plusmouse <plusmouse@protonmail.com>
2023-12-28 12:24:15 +0000

    Visible options in sorting dropdown for legacy sort modes

--- CustomiseDialog/Main.lua
+++ CustomiseDialog/Main.lua
@@ -633,6 +633,8 @@ function BaganatorCustomiseDialogMixin:SetupSorting()
   local allModes = {
     {"type", BAGANATOR_L_ITEM_TYPE},
     {"quality", BAGANATOR_L_ITEM_QUALITY},
+    {"type-legacy", BAGANATOR_L_ITEM_TYPE_LEGACY},
+    {"quality-legacy", BAGANATOR_L_ITEM_QUALITY_LEGACY},
     {"combine_stacks_only", BAGANATOR_L_COMBINE_STACKS_ONLY},
     {"blizzard", BAGANATOR_L_BLIZZARD},
     {"expansion", BAGANATOR_L_EXPANSION},
