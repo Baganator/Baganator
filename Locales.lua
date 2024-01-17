7258d98e210ac8802c22979a995eb27ae27ab3dc
plusmouse <plusmouse@protonmail.com>
2024-01-17 08:11:08 +0000

    Simplify tooltip message for transfer button at bank from/to bags

--- Locales.lua
+++ Locales.lua
@@ -164,7 +164,9 @@ L["SCRAPPING_MACHINE"] = "Scrapping Machine"
 
 L["TRANSFER"] = "Transfer"
 L["TRANSFER_BANK_VIEW_TOOLTIP_TEXT"] = "Move searched for items out of the bank.\n\nSpecial action on right-click, transfer items where the same items are already in the bags"
+L["TRANSFER_BANK_VIEW_TOOLTIP_TEXT_SHORT"] = "Move searched for items out of the bank into the bags."
 L["TRANSFER_MAIN_VIEW_BANK_TOOLTIP_TEXT"] = "Move searched for items out from the bags into the bank.\n\nSpecial actions on right-click (transfer items already in the bank) and shift+click (rearrange so exactly one stack of everything is in the bag)."
+L["TRANSFER_MAIN_VIEW_BANK_TOOLTIP_TEXT_SHORT"] = "Move searched for items out from the bags into the bank."
 L["TRANSFER_MAIN_VIEW_MAIL_TOOLTIP_TEXT"] = "Attach the searched for items to a mail for sending.\n\nRight-click to clear all attachments."
 L["TRANSFER_MAIN_VIEW_SCRAPPER_TOOLTIP_TEXT"] = "Place scrappable items into the scrapping machine.\n\nRight-click to remove all items from the scrapping machine."
 
