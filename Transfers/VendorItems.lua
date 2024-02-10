b1eeced1d2ebccead5b5ede1c9aab02123794b72
plusmouse <plusmouse@protonmail.com>
2024-02-10 19:02:29 +0000

    Move vendor transfer code into Transfers folder

---
+++ Transfers/VendorItems.lua
@@ -0,0 +1,20 @@
+function Baganator.Transfers.VendorItems(toSell)
+  local sold = 0
+  for _, item in ipairs(toSell) do
+    local location = ItemLocation:CreateFromBagAndSlot(item.bagID, item.slotID)
+    if C_Item.DoesItemExist(location) and not item.hasNoValue then
+      C_Container.UseContainerItem(item.bagID, item.slotID)
+      sold = sold + 1
+      -- Limit sold to the most that can be bought back from the merchant if
+      -- clicked by mistaked
+      if sold >= 12 then
+        return
+      end
+    end
+  end
+  if sold == 0 then
+    UIErrorsFrame:AddMessage(BAGANATOR_L_THE_MERCHANT_DOESNT_WANT_ANY_OF_THOSE_ITEMS, 1.0, 0.1, 0.1, 1.0)
+  end
+
+  return Baganator.Constants.SortStatus.Complete
+end
--- UnifiedBags/BagTransfers.lua
+++ UnifiedBags/BagTransfers.lua
@@ -75,28 +75,11 @@ RegisterBagTransfer(
   false, BAGANATOR_L_TRANSFER_MAIN_VIEW_SCRAPPER_TOOLTIP_TEXT
 )
 
-local function VendorItems(matches, characterName, callback)
-  local sold = 0
-  for _, item in ipairs(matches) do
-    local location = ItemLocation:CreateFromBagAndSlot(item.bagID, item.slotID)
-    if C_Item.DoesItemExist(location) and not item.hasNoValue then
-      C_Container.UseContainerItem(item.bagID, item.slotID)
-      sold = sold + 1
-      -- Limit sold to the most that can be bought back from the merchant if
-      -- clicked by mistaked
-      if sold >= 12 then
-        return
-      end
-    end
-  end
-  if sold == 0 then
-    UIErrorsFrame:AddMessage(BAGANATOR_L_THE_MERCHANT_DOESNT_WANT_ANY_OF_THOSE_ITEMS, 1.0, 0.1, 0.1, 1.0)
-  end
-  callback(Baganator.Constants.SortStatus.Complete)
-end
-
 RegisterBagTransfer(
   function() return C_PlayerInteractionManager.IsInteractingWithNpcOfType(Enum.PlayerInteractionType.Merchant) end,
-  VendorItems,
+  function(matches, characterName, callback)
+    local status = Baganator.Transfers.VendorItems(matches)
+    callback(status)
+  end,
   true, BAGANATOR_L_TRANSFER_MAIN_VIEW_MERCHANT_TOOLTIP_TEXT
 )
