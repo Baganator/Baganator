034cfe290ce908f9f0dbb5046140fc4aad40e96e
plusmouse <plusmouse@protonmail.com>
2024-02-15 13:44:11 +0000

    When using Transfer to vendor skip dialog for tradeable items with time limit

--- Transfers/VendorItems.lua
+++ Transfers/VendorItems.lua
@@ -1,4 +1,11 @@
+local frame = CreateFrame("Frame")
+-- Avoid confirmation dialogs for sales to vendors
+frame:SetScript("OnEvent", function()
+  SellCursorItem()
+end)
+
 function Baganator.Transfers.VendorItems(toSell)
+  frame:RegisterEvent("MERCHANT_CONFIRM_TRADE_TIMER_REMOVAL")
   local sold = 0
   for _, item in ipairs(toSell) do
     local location = ItemLocation:CreateFromBagAndSlot(item.bagID, item.slotID)
@@ -15,6 +22,7 @@ function Baganator.Transfers.VendorItems(toSell)
   if sold == 0 then
     UIErrorsFrame:AddMessage(BAGANATOR_L_THE_MERCHANT_DOESNT_WANT_ANY_OF_THOSE_ITEMS, 1.0, 0.1, 0.1, 1.0)
   end
+  frame:UnregisterEvent("MERCHANT_CONFIRM_TRADE_TIMER_REMOVAL")
 
   return Baganator.Constants.SortStatus.Complete
 end
