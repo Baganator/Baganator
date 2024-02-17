4ac6cc0905be7713da3375e33ab29347e2fd89f6
plusmouse <plusmouse@protonmail.com>
2024-02-17 18:35:51 +0000

    Apply show character race icons option to currency tooltip summaries

--- InventoryTracking/Tooltips.lua
+++ InventoryTracking/Tooltips.lua
@@ -189,7 +189,7 @@ function Baganator.Tooltips.AddCurrencyLines(tooltip, currencyID)
     if s.className then
       character = RAID_CLASS_COLORS[s.className]:WrapTextInColorCode(character)
     end
-    if s.race then
+    if Baganator.Config.Get(Baganator.Config.Options.SHOW_CHARACTER_RACE_ICONS) and s.race then
       character = Baganator.Utilities.GetCharacterIcon(s.race, s.sex) .. " " .. character
     end
     tooltip:AddDoubleLine("  " .. character, WHITE_FONT_COLOR:WrapTextInColorCode(FormatLargeNumber(s.quantity)))
