ff7c7d1712265b3d419be4f83434ac10d82abb42
plusmouse <plusmouse@protonmail.com>
2024-05-26 10:04:39 +0100

    Fix entire backpack view frame table being stored in saved variables

--- ItemViewCommon/Initialize.lua
+++ ItemViewCommon/Initialize.lua
@@ -5,9 +5,16 @@ local function SetupCharacterSelect()
 
   local function SetPositions()
     characterSelect:ClearAllPoints()
-    local anchor = Baganator.Config.Get(Baganator.Config.Options.CHARACTER_SELECT_POSITION)
+    -- Fix for setting storing frame instead of just the frame name in previous
+    -- versions
+    local setting = Baganator.Config.Get(Baganator.Config.Options.CHARACTER_SELECT_POSITION)
+    if type(setting[2]) == "table" then
+      setting[2] = nil
+    end
+    local anchor = CopyTable(setting)
     if _G[anchor[2]] == nil then -- Accommodate renamed backpack frames
       anchor[2] = Baganator_SingleViewBackpackViewFrame or UIParent
+      setting[2] = anchor[2]:GetName()
     end
     characterSelect:SetPoint(unpack(anchor))
   end
