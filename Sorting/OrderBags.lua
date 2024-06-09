bca811deaa7df67201b606bc184f702905dc0508
plusmouse <plusmouse@protonmail.com>
2024-06-09 18:28:49 +0100

    Prevent sorting when the player is dead

--- Sorting/OrderBags.lua
+++ Sorting/OrderBags.lua
@@ -164,7 +164,7 @@ end
 -- ignoreCount: Number of slots to ignore in the regular bag (start or end
 -- depending on ignoreAtEnd)
 function Baganator.Sorting.ApplyBagOrdering(bags, bagIDs, indexesToUse, bagChecks, isReverse, ignoreAtEnd, ignoreCount)
-  if InCombatLockdown() then -- Sorting breaks during combat due to Blizzard restrictions
+  if InCombatLockdown() or UnitIsDead("player") then -- Sorting breaks during combat due to Blizzard restrictions
     return Baganator.Constants.SortStatus.Complete
   end
 
