9300d586833468e9cbcf818ec6fc105fe5789084
plusmouse <plusmouse@protonmail.com>
2024-08-02 10:30:56 +0100

    Vanilla: Don't access "blizzard" equipment manager APIs on classic era

--- API/EquipmentSets.lua
+++ API/EquipmentSets.lua
@@ -1,13 +1,9 @@
 local _, addonTable = ...
 -- Blizzard Equipment sets (Wrath onwards)
-do
+if not addonTable.Constants.IsEra and (not addonTable.Constants.IsRetail or not IsMacClient()) then
   local BlizzardSetTracker = CreateFrame("Frame")
 
   function BlizzardSetTracker:OnLoad()
-    if addonTable.Constants.IsRetail and IsMacClient() then
-      return
-    end
-
     FrameUtil.RegisterFrameForEvents(self, {
       "BANKFRAME_OPENED",
       "EQUIPMENT_SETS_CHANGED",
