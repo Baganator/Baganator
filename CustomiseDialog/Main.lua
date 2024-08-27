5ca932398f44e38d25ab3ed4e1253826176b861b
plusmouse <plusmouse@protonmail.com>
2024-08-27 21:21:51 +0100

    Customise: Categories: Prevent duplicate enable categories dialogs

--- CustomiseDialog/Main.lua
+++ CustomiseDialog/Main.lua
@@ -995,6 +995,7 @@ function BaganatorCustomiseDialogMixin:SetupCategoriesOptions()
   addonTable.CallbackRegistry:RegisterCallback("SettingChanged", function(_, settingName)
     if frame:IsVisible() and tIndexOf(addonTable.CategoryViews.Constants.RedisplaySettings, settingName) ~= nil then
       if addonTable.Config.Get("bag_view_type") ~= "category" and addonTable.Config.Get("bank_view_type") ~= "category" then
+        StaticPopup_Hide(enableDialog)
         StaticPopup_Show(enableDialog)
       end
     end
