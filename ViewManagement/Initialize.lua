a12c5748e1a8459d4c1be261852d8616f90d7467
plusmouse <plusmouse@protonmail.com>
2024-09-02 18:56:49 +0100

    Use BlizzMove IgnoreClamping parameter

--- ViewManagement/Initialize.lua
+++ ViewManagement/Initialize.lua
@@ -14,6 +14,7 @@ local function RegisterForScaling(frame)
         ['Baganator'] = {
            [frame:GetName()] = {
              NonDraggable = true,
+             IgnoreClamping = true,
            },
         },
     });
