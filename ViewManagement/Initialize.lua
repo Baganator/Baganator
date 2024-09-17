91135a019463be8f2dbef7160f319b8b9c1b903b
plusmouse <plusmouse@protonmail.com>
2024-09-18 00:25:23 +0100

    [Fixes #291] Revert Shift+Click currency fix due to lag when entering Delves

--- ViewManagement/Initialize.lua
+++ ViewManagement/Initialize.lua
@@ -440,8 +440,14 @@ function addonTable.ViewManagement.Initialize()
   xpcall(function()
     local info = C_XMLUtil.GetTemplateInfo("BackpackTokenTemplate")
     local tokenWidth = info and info.width or 50
-    BackpackTokenFrame:UnregisterEvent("CURRENCY_DISPLAY_UPDATE")
-    BackpackTokenFrame:SetWidth(tokenWidth * addonTable.Constants.MaxPinnedCurrencies + 1) -- Support tracking up to 100 currencies
+    -- Reverts token frame width change after using the character frame to avoid
+    -- unexpected freezes.
+    TokenFramePopup:HookScript("OnShow", function()
+      BackpackTokenFrame:SetWidth(tokenWidth * addonTable.Constants.MaxPinnedCurrencies + 1) -- Support tracking up to 100 currencies
+    end)
+    TokenFramePopup:HookScript("OnHide", function()
+      BackpackTokenFrame:SetWidth(tokenWidth * 3 + 1)
+    end)
   end, CallErrorHandler)
 
   xpcall(function()
