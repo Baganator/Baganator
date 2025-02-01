18a0f3edcde03e62d025181d94a74d395b0a785e
plusmouse <plusmouse@protonmail.com>
2025-02-01 20:08:11 +0000

    Transfers: Prevent trade timer removal dialog intefering with vendoring

--- Transfers/VendorItems.lua
+++ Transfers/VendorItems.lua
@@ -8,12 +8,6 @@ local lastTime = 0
 local shiftLock = false
 local shiftTime = 0
 
--- Avoid confirmation dialogs for sales to vendors
-local frame = CreateFrame("Frame")
-frame:SetScript("OnEvent", function(_, eventName)
-  SellCursorItem()
-end)
-
 function addonTable.Transfers.VendorItems(toSell)
   if GetTimePreciseSec() - lastTime < 1 then
     shiftTime = GetTimePreciseSec()
@@ -22,13 +16,14 @@ function addonTable.Transfers.VendorItems(toSell)
     shiftLock = false
   end
 
-  frame:RegisterEvent("MERCHANT_CONFIRM_TRADE_TIMER_REMOVAL")
   local sold = 0
   local breakIndex
   for index, item in ipairs(toSell) do
     local location = ItemLocation:CreateFromBagAndSlot(item.bagID, item.slotID)
     if C_Item.DoesItemExist(location) and not C_Container.GetContainerItemInfo(item.bagID, item.slotID).hasNoValue then
-      C_Container.UseContainerItem(item.bagID, item.slotID)
+      ClearCursor()
+      C_Container.PickupContainerItem(item.bagID, item.slotID)
+      SellCursorItem()
       sold = sold + 1
       if sold >= 6 then
         breakIndex = index
@@ -39,7 +34,6 @@ function addonTable.Transfers.VendorItems(toSell)
   if sold == 0 then
     UIErrorsFrame:AddMessage(BAGANATOR_L_THE_MERCHANT_DOESNT_WANT_ANY_OF_THOSE_ITEMS, 1.0, 0.1, 0.1, 1.0)
   end
-  frame:UnregisterEvent("MERCHANT_CONFIRM_TRADE_TIMER_REMOVAL")
 
   waitingOnMoney = sold
   lastTime = GetTimePreciseSec()
