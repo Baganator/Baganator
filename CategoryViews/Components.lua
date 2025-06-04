ac5b4301ffae53163311552297a68b872cce0b92
plusmouse <plusmouse@protonmail.com>
2025-06-04 14:52:18 +0100

    Fix Shift+Click to collapse only applying to first child

--- CategoryViews/Components.lua
+++ CategoryViews/Components.lua
@@ -112,6 +112,7 @@ function addonTable.CategoryViews.GetSectionButtonPool(parent)
             else
               local sectionSource = entry:match("^_(.*)")
               if sectionSource then
+                level = level + 1
                 sectionToggled[sectionSource] = sectionToggled[self.source]
               end
             end
