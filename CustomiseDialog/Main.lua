5e231629834b13754988ef0e183125122787b6b8
plusmouse <plusmouse@protonmail.com>
2025-08-12 14:49:56 +0100

    Retail: Increase ignored slots limit to account for bigger bags

--- CustomiseDialog/Main.lua
+++ CustomiseDialog/Main.lua
@@ -357,7 +357,7 @@ local SORTING_OPTIONS = {
     min = 0,
     max = 128,
     lowText = "0",
-    highText = "128",
+    highText = "240",
     text = addonTable.Locales.IGNORED_BAG_SLOTS,
     option = "sort_ignore_slots_count_2",
   },
